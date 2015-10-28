cd31578305bf931ecd0006de965cfbccf3e517ad
Lee R. Boynton <lee@boynton.com>
2015-10-28 14:09:22 -0700

    Created

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	pPort := flag.String("p", "23000", "select port to listen on (default is 23000)")
+	listener, err := net.Listen("tcp", ":"+*pPort)
+	if err != nil {
+		fmt.Println("Cannot listen: ", err)
+	} else {
+		for {
+			con, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Cannot accept: ", err)
+			} else {
+				go func(con net.Conn) {
+					peer, _, _ := net.SplitHostPort(con.RemoteAddr().String())
+					fmt.Println(peer, ": Accepted")
+					for {
+						message, err := bufio.NewReader(con).ReadString('\n')
+						if err != nil {
+							fmt.Println(peer, ": Closed")
+							return
+						}
+						fmt.Print(peer, ": Received message ", string(message))
+						newmessage := strings.ToUpper(message)
+						con.Write([]byte(newmessage))
+					}
+				}(con)
+			}
+		}
+	}
+}
